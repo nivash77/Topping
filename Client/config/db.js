cc6cd7f3ffd94f0418c24eb02c0521d4cdbeaa66
nivash77 <151871666+nivash77@users.noreply.github.com>
2025-04-05 13:01:18 +0530

    Add files via upload

--- Client/config/db.js
+++ Client/config/db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose= require('mongoose')
-const mongourl= process.env.MONGO||"mongodb+srv://admin:admin@nivash.nrt23.mongodb.net/?retryWrites=true&w=majority&appName=nivash"
+const mongourl= process.env.MONGO
 mongoose.connect(mongourl)
     .then(() => console.log('DB connected'))
     .catch(err => console.log('DB error:', err));
